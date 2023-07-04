9481ba8e6d7792b01b8c36a12fa5f082e39cec5c
Mbokola <tonypeaks@gmail.com>
2023-07-04 13:22:47 +0000

    Task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-/hbt{2,5}n/
+# store user input
+usr_input = ARGV[0]
+# store regex pattern
+pattern = /hbt{2,5}n/
+# use scan to find pattern in user input
+matching = usr_input.scan(pattern)
+#ouput joined match
+puts matching.join
