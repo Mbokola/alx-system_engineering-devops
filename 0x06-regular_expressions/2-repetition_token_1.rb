33c65a7feb32e7a30852a30662e2052fe019eec6
Mbokola <tonypeaks@gmail.com>
2023-07-04 13:47:49 +0000

    Task 2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -2,7 +2,7 @@
 # store user input
 usr_input = ARGV[0]
 # store regex pattern
-pattern = /hb[tn]+/
+pattern = /hb?t?n/
 # use scan to find pattern in user input
 matching = usr_input.scan(pattern)
 #ouput joined match
