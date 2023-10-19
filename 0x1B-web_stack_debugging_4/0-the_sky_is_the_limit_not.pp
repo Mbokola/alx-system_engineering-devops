0cb993107055e3ed17d0407b94d282446d7bda8c
Mbokola <tonypeaks@gmail.com>
2023-10-19 21:35:53 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
+# Increases the amount of traffic an Nginx server can handle.
+
 # Increase the ULIMIT of the default file
-exec { 'fix-for-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => "test \"$(cat /etc/default/nginx | grep '15')\" != ''",
-  require => Package['nginx'],  # Ensure nginx package is installed
-}
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
 # Restart Nginx
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Exec['fix-for-nginx'],  # Subscribe to the previous exec
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
